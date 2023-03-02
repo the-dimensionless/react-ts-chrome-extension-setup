fa668c4d6afb99e33ab112408506d15830dcb0c6
the-dimensionless <sumit.singh1@brillio.com>
2023-03-02 19:52:10 +0530

    base setup

---
+++ readMe.md
@@ -0,0 +1,12 @@
+### TS, React & Webpack Build system
+ 
+ 1. Install Node => npm init
+ 2. npm i react@latest
+ 3. npm i typescript -g
+ 4. npm config get prefix => set this path to enable global tsc
+ 5. create tsconfig.json => specifies the root files and the compiler options required to compile the project
+ 6. Webpack => Module Bundler
+ 7. npm i ts-loader
+ 8. define webpack config
+ 9. npm i webpack, webpack-cli
+ 10. npm i copy-webpack-plugin to copy static files into dist
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/test.tsx',
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                test: /\.tsx?$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve('./src/manifest.json'),
+                    to: path.resolve('dist')
+                }
+            ]
+        })
+
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
